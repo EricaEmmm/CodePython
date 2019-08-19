1e92c86af26dc81332046a9d806f9e2960dbcc0b
EricaEmmm <1016920795@qq.com>
2019-08-19 14:04:23 +0800

    0819

--- LeetCode/链表_23_合并K个排序链表.py
+++ LeetCode/链表_23_合并K个排序链表.py
@@ -13,9 +13,71 @@
 
 from tool import ListNode, createLinkList, printList
 
+'''
+复杂度分析（假设K个链表，所有链表总共N个节点）
+暴力：所有链表节点放到一个数组中，排序，再创建新链表
+    时间：O(NlogN)，空间：O(N)
+逐一比较：比较每个链表首节点，最小的放到最终有序链表尾部。几乎每个节点都要比较K-1次
+    时间：O(NK)，空间：O(1)
+优先队列：比较环节用优先队列（最小堆）优化，比较代价O(logK)
+    时间：O(NlogK)，空间：O(K)
+分治：将K个链表一直对半分，向上两两合并。每次合并都要遍历几乎全部N个节点，合并logK次
+    时间：O(NlogK)，空间：O(NlogK)
+'''
+
 class Solution(object):
+    '''
+    分治
+    时间复杂度：O(NlogK)
+    '''
+    def mergeKLists1(self, lists):
+        if len(lists) == 1:
+            return lists[0]
+        mid = len(lists) // 2
+        l1 = self.mergeKLists1(lists[:mid])
+        l2 = self.mergeKLists1(lists[mid:])
+        return self.mergeTwoLists(l1, l2)
+
+    def mergeTwoLists(self, l1, l2):
+        if not l1: return l2
+        if not l2: return l1
+        if l1.val < l2.val:
+            newHead = l1
+            newHead.next = self.mergeTwoLists(l1.next, l2)
+        else:
+            newHead = l2
+            newHead.next = self.mergeTwoLists(l1, l2.next)
+        return newHead
+
+    '''
+    优先队列
+    时间复杂度：O(NlogK)
+    '''
+    def mergeKLists2(self, lists):
+        from queue import PriorityQueue
+        que = PriorityQueue()
+        # 建立头结点优先队列
+        for i in lists:
+            if i:
+                que.put(i)
+        # 构建新有序链表
+        head = ptr = ListNode(None)
+        while que:
+            idx = que.get()
+            ptr.next = idx
+            ptr = ptr.next
+            idx = idx.next
+            if idx:
+                que.put(idx)
+        return head.next
+
     def mergeKLists(self, lists):
+        return self.mergeKLists2(lists)
 
 
 if __name__ == '__main__':
     s = Solution()
+    l1 = createLinkList([1, 4, 5])
+    l2 = createLinkList([1, 3, 4])
+    l3 = createLinkList([2, 6])
+    printList(s.mergeKLists([l1, l2, l3]))
